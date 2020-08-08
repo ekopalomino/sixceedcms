848731a52798bef319541073d260684ef41d77b0
Heru Palomino SIXCEED <heru.palomino@gmail.com>
2020-08-08 08:11:48 +0700

    Header Access Based Site

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -6,45 +6,72 @@
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="{{ route('config.index') }}" class="nav-link {{set_active(['config.index','user.index','roles.index','roles.create','log.index','country.index','region.index','city.index','dutycat.index','articlecat.index','faqcat.index']) }}">Konfig</a>
 		</li>
+		@if((auth()->user()->site_id) == '8689caab-d1d0-4b99-a814-d6e2d0b56361')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="{{route('main.index') }}" class="nav-link {{set_active(['main.index']) }}">Situs Utama</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == 'bc5397fc-f875-4326-846f-315e6e6fd0ba')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Ditjen PEN</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '4c565b80-f7e9-4421-81be-a831ae48edf2')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Ditjen PPI</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '745572a6-c3a9-491a-b069-966d9cfa7054')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Ditjen PLN</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == 'e2fa0b3f-84e2-4046-ae68-fe5f987cbe7b')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Ditjen PKTN</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '85cd37d3-e818-4e04-b294-3491feb60285')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Ditjen PDN</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '745572a6-c3a9-491a-b069-966d9cfa7054')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Ditjen Daglu</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '92876445-2b7c-4e2f-bb43-d3b71b608e4e')
 		<li class="nav-item d-none d-sm-inline-block">
-			<a href="#" class="nav-link ">PPSDK</a>
+			<a href="#" class="nav-link {{set_active(['foto.index','video.index','fnban.index','fnpub.index','about.index','about.create','about.edit','duty.index','duty.create','duty.edit','strat.index','post.index','write.create','upload.create','post.edit',
+					'postSearch.index','postQuery.index','faq.index','faq.create','faq.edit','event.index','event.create','event.edit']) }}">Pusat Pengembangan Sumber Daya Kemetrologian</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '2da488d4-a8e7-499a-b6e7-360ff17f9585')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">BPPP</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == '5a7826cf-e620-460f-9bfb-ef2adacf6471')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Rovodag</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == 'd59ba15e-5abf-4572-9992-64950d4f65ea')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">KPPI</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == 'ee5c63f5-58b4-4ca5-9acb-09de683f6549')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">KADI</a>
 		</li>
+		@endif
+		@if((auth()->user()->site_id) == 'a0fe8237-c432-4d27-8edd-669cb9719278')
 		<li class="nav-item d-none d-sm-inline-block">
 			<a href="#" class="nav-link ">Itjen</a>
 		</li>
+		@endif
 	</ul>
 	<ul class="navbar-nav ml-auto">
 		<li class="nav-item dropdown">
