b69668f49653aed510cbe2d97e0cbf55a8f9fc52
Eko Heru. Wibowo <eko.heru@agrinesia.net>
2020-11-23 16:10:28 +0700

    update

---
+++ resources/views/backend/dashboard/djpen.blade.php
@@ -0,0 +1,7 @@
+@extends('backend.layout.main')
+@section('header.title')
+Kementerian Perdagangan Republik Indonesia | Direktorat Jenderal Pengembangan Ekspor Nasional
+@endsection
+@section('content')
+Dashboard DITJEN PEN
+@endsection
---
+++ resources/views/backend/dashboard/kadi.blade.php
@@ -0,0 +1,7 @@
+@extends('backend.layout.main')
+@section('header.title')
+Kementerian Perdagangan Republik Indonesia | Komite Anti Dumping
+@endsection
+@section('content')
+Dashboard KADI
+@endsection
